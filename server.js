c425464d5c08c9f34ed4b66498691d9ef5929119
Daniel Echeverri <echeverri121@gmail.com>
2023-10-13 13:09:53 -0500

    First changes in server.js

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
 
 module.exports = app;
 
@@ -9,17 +12,19 @@ module.exports = app;
 const PORT = process.env.PORT || 4001;
 
 // Add middleware for handling CORS requests from index.html
-
+app.use(cors());
 
 // Add middware for parsing request bodies here:
-
+app.use(bodyParser.json());
 
 // Mount your existing apiRouter below at the '/api' path.
 const apiRouter = require('./server/api');
-
+apiRouter.use()
 
 // This conditional is here for testing purposes:
 if (!module.parent) { 
-  // Add your code to start the server listening at PORT below:
+  app.listen(PORT, ()=> {
+    console.log(`Listening at port ${PORT}`);
+  })
 
 }
--- server/api.js
+++ server/api.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const apiRouter = express.Router();
-
+apiRouter.use((req, res, next) => {
+    console.log('Request recieved');
+    next();
+});
 
 
 module.exports = apiRouter;
